cd85f4e7363e072142022beb8f62412a6dc88459
David Fuka <dfuka@revspringinc.com>
2014-02-04 14:19:57 -0700

    Create Guidebook

---
+++ model/ChapterModel.js
@@ -0,0 +1,24 @@
+Guidebook.service('ChapterModel', function(){
+  this.getChapters = function(){
+  return [{
+    id: 0,
+    title: "Chapter 1: So, what is AngularJS?",
+    summary: "Find out what separates..."
+  }, {
+    id: 1,
+    title: "Chapter 2: HelloWorld",
+    summary: "Up and running"
+  }, {
+    id: 2,
+    title: "Chapter 3: MVC",
+    summary: "Learn MVC"
+  }, {
+    id: 3,
+    title: "Chapter 4: Key features",
+    summary: "Strengths"
+  }, {
+    id: 4,
+    title: "Chapter 5: Community",
+    summary: "Top contributors"
+  }]};
+});
